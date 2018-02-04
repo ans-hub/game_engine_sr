c3079423889088be5c1965f67a215703a1a34d12
Anton Novoselov <ans.novoselov@gmail.com>
2018-02-04 12:19:27 +0300

    Add lib data

---
+++ lib/data/ply_loader.cc
@@ -0,0 +1,353 @@
+// *************************************************************
+// File:    ply_loader.cc
+// Descr:   ply (ascii) format loader
+// Author:  Novoselov Anton @ 2018
+// URL:     https://github.com/ans-hub/ply_loader
+// *************************************************************
+
+#include "ply_loader.h"
+
+namespace ply {
+
+// Main loads procedure (ascii) 
+
+bool Loader::Load(std::istream& iss)
+{
+  ClearData();
+  helpers::RewindStream(iss);
+  
+  std::string line {};
+  std::getline(iss, line);
+
+  if (line != "ply")
+  {
+    iss.setstate(std::ios::failbit);
+    throw ply::Except("Load(): not a ply file");
+  }
+
+  LoadHeader(iss);
+  LoadData(iss);
+
+  return true;
+}
+
+// Reads header of ascii ply file (iss pos should stay at the data pos)
+
+void Loader::LoadHeader(std::istream& iss)
+{
+  std::string line {""};                // to store line of istream
+  std::string word {""};                // to store words of line
+  std::string name {""};                // to store prop name of line
+  std::string elem {""};                // to store elem name of line
+  std::string type {""};                // to store type of property
+  int order {0};                        // to store pos of curr elem in data_ arr
+  int num {0};                          // to store input integer
+  int pos {0};                          // current prop pos in prop array
+  int prop {0};                         // current prop array of element
+
+  // Read stream line by line
+
+  while(std::getline(iss, line))
+  {
+    std::istringstream stream {line};   // to read word by word
+    stream >> word;                     // get word
+    
+    // Fills element`s general info
+
+    if (word == "element")              // if first word is "element"
+    {
+      stream >> elem >> num;            // get line like "vertex 26"
+      props_.emplace_back();            // allocate new properties describer
+      sizes_.push_back(num);            // add elem count
+      data_.emplace_back();             // add new element
+      data_.back().resize(num);         // and resize it for store num of items
+      head_[elem].count_ = num;         // set name and count of element
+      order_[elem] = order;             // remember the order of element
+      pos = 0;
+      prop = 0;
+      ++order;
+    }
+
+    // Fill element`s sequence of properties
+
+    else if (word == "property")
+    {
+      stream >> type;
+      if (type == "list")               // add next property of element
+      {
+        stream >> type >> type >> name;
+        pos = 0;
+        if (prop != 0) ++prop;
+        head_[elem].list_props_[name] = std::make_tuple(type, prop);
+        ++prop;
+        props_.back().push_back('l');
+      }
+      else
+      {
+        stream >> name;
+        head_[elem].single_props_[name] = std::make_tuple(type, prop, pos);
+        ++pos;
+        props_.back().push_back('d');
+      }
+    }
+    else if (word == "end_header")      // if we stay in the end of header
+      break;
+  }
+}
+
+// Returns raw data of defined element (by its name)
+
+cVector3d& Loader::GetRawData(const std::string& key)
+{
+  if (helpers::IsKeyAbsentInMap(key, order_))
+    throw Except(string{"GetRawData(): elem absent - "} + string{key});
+  return data_[order_[key]];
+}
+
+// Returns line of filtered properties of preloaded ply
+
+Vector2d Loader::GetLine(const std::string& elem, const Vector1s& filter)
+{
+  // Check if given elem is valid
+
+  if (helpers::IsKeyAbsentInMap(elem, order_))
+    throw Except(string{"GetLine(): elem absent - "} + string{elem});
+  
+  // Check if given filer elements are valid
+
+  for (const auto& prop : filter)
+    if (helpers::IsKeyAbsentInMap(prop, head_[elem].single_props_))
+      throw Except(string{"GetLine(): property absent - "} + string{elem});
+
+  // Predefine element with which we work, and create result var
+
+  auto& curr = data_[order_[elem]];
+  Vector2d res (curr.size());
+  
+  // Iterate through each item of current element
+
+  for (std::size_t i = 0; i < curr.size(); ++i)
+  {
+    // Fill current result item with requested property
+    
+    res[i].reserve(filter.size());
+    for (auto& f : filter)
+    {
+      int prop_arr = std::get<1>(head_[elem].single_props_[f]); // prop_arr pos
+      int prop_pos = std::get<2>(head_[elem].single_props_[f]); // elem_pos in prop_arr
+      res[i].push_back(curr[i][prop_arr][prop_pos]);
+    }
+  }
+  return res;
+}
+
+// Returns propeties of element represents by list
+
+Vector2d Loader::GetList(const std::string& elem, const std::string& filter)
+{
+  // Check if given elem is valid
+
+  if (helpers::IsKeyAbsentInMap(elem, order_))
+    throw Except(string{"GetLine(): elem absent - "} + string{elem});
+
+  // Check if given filer elements are valid
+
+  if (helpers::IsKeyAbsentInMap(filter, head_[elem].list_props_))
+    throw Except(string{"GetLine(): property absent - "} + string{filter});
+
+  // Predefine element with which we work, and create result var
+
+  auto& curr = data_[order_[elem]];
+  Vector2d res (curr.size());
+
+  // Iterate through each item of current element
+
+  for (std::size_t i = 0; i < curr.size(); ++i)
+  {
+    // Fill current result item with requested property
+        
+    int prop_arr = std::get<1>(head_[elem].list_props_[filter]); // prop_arr pos
+    res[i] = curr[i][prop_arr];
+  }
+  return res;
+}
+
+//************************************************************************
+// PRIVATE MEMBER FUNCTIONS IMPLEMENTATION
+//************************************************************************
+
+// Reads data after header (istream pos shoudld stay at the data pos)
+// First would meets elements which were described first in the header.
+//
+// I.e.:
+//  header section: "vertex 20", "face 12" (element and items count)
+//  data in stream: first 20 lines is vertexes, next 12 lines is faces
+//  data in class:  data_[0] is 20 vertexes, data_[1] is 12 faces
+//
+// Each data_[elem][item] contains sub arrays, which represents properties.
+// I.e. 1:  header section: "vertex 10" +6 single properties
+//          data in class:  data_[0][item][0][0..5]
+// I.e. 2:  header section: "face 6" +1 list, +2 single properties, +1 list
+//          data in class:  data_[1][item][0][size of first list]
+//                          data_[1][item][1][0..1]
+//                          data_[2][item][2][size of second list]
+// I.e. 3:  header section: "edge 8" +1 list
+//          data in class:  data_[2][item][0][size of first list]
+
+void Loader::LoadData(std::istream& iss)
+{
+  std::string line {""};  // user to store line of stream
+  std::string word {""};  // used to store word from line 
+  int    ival {0};        // used to store integer value from stream
+  double dval {0};        // used to store double value from stream
+  int    elem {0};        // element type index (vertex, face, etc...)
+  int    item {0};        // item index of curr element 
+
+  int total_sizes {0};    // needs to check is data lines is equal sizes
+  for (const auto& sz : sizes_)
+    total_sizes += sz;
+
+  while(std::getline(iss, line))
+  {
+    std::istringstream stream {line};   // to read word by word
+    
+    if (sizes_[elem] == 0) {            // if we have read all data of curr elem
+      ++elem;                           //  then go to the next element
+      item = 0;
+    }
+    
+    // Reserve max space to store arrays of properties values
+
+    data_[elem][item].reserve(props_[elem].size());
+
+    // Now we have data line. We should read it and understand which type of
+    // property we read now. Basing on this knowledge we read all words in line
+
+    char last_prop {'l'};
+    for (auto& prop : props_[elem])
+    {
+      // If this is the "list", then add new property array to data_[elem][item]
+
+      if (prop == 'l')
+      {                                 
+        last_prop = 'l';
+        if (!(stream >> ival))          // get num of sub properties in curr list
+          helpers::ThrowLoadDataError(elem, item);
+
+        data_[elem][item].emplace_back();
+        data_[elem][item].back().reserve(ival);
+        
+        while (ival)                    // reads all sub properties in the list
+        {
+          if (!(stream >> dval))
+            helpers::ThrowLoadDataError(elem, item);
+
+          data_[elem][item].back().push_back(dval);
+          --ival;
+        }
+      }
+      
+      // If this is the single property and previous was a "list", then add
+      // new property array to data_[elem][item], else just fill back()
+
+      else if (prop == 'd')
+      {
+        if (last_prop == 'l') {
+            data_[elem][item].emplace_back();
+            data_[elem][item].back().reserve(props_[elem].size());
+          last_prop = 'd';
+        }
+        if (!(stream >> dval))
+          helpers::ThrowLoadDataError(elem, item);
+        data_[elem][item].back().push_back(dval);
+      }
+    }
+
+    // Go to the next item and reduce curr elem sizes
+
+    ++item;
+    --sizes_[elem];
+    --total_sizes;
+  }
+
+  if (total_sizes != 0)
+    throw Except("LoadData(): sizes in header is not equal fact");
+}
+
+// Clears all internal data
+
+void Loader::ClearData()
+{
+  props_ = Vector2c {};
+  sizes_ = Vector1i {};
+  data_ = Vector4d {};
+  head_ = MHeader {};
+  order_ = MapSI {};
+  filter_.clear();
+}
+
+//************************************************************************
+// Helper functions inplementation
+//************************************************************************
+
+// Rewinds istream
+
+void helpers::RewindStream(std::istream& iss)
+{
+  iss.clear();
+  iss.seekg(0);
+}
+
+// Throws load data error
+
+void helpers::ThrowLoadDataError(int elem_type, int item_num)
+{
+  std::ostringstream oss {};
+  oss << "LoadData(): invalid data in "
+      << "elem " << elem_type << ' ' 
+      << "item " << item_num;
+  throw Except(oss.str().c_str());
+}
+
+// Out map of headers
+
+std::ostream& helpers::operator<<(std::ostream& oss, const MHeader& header)
+{
+  for (const auto& elem : header)
+  {
+    oss << "Element: " << elem.first << ", ";
+    oss << elem.second;
+  }
+  return oss;
+}
+
+// Out element header
+
+std::ostream& helpers::operator<<(std::ostream& oss, const Header& h)
+{
+  oss << "count: " << h.count_ << '\n';
+
+  if (!h.single_props_.empty())
+    oss << "  Single properies: \n";
+ 
+  for (const auto& prop : h.single_props_)
+  {
+    oss << "    name: " << prop.first << ", "; 
+    oss << "type: " << std::get<0>(prop.second) << ", ";
+    oss << "arr: " << std::get<1>(prop.second) << ", ";
+    oss << "pos: " << std::get<2>(prop.second) << '\n';
+  }
+
+  if (!h.list_props_.empty())
+    oss << "  List properies: \n";
+
+  for (const auto& prop : h.list_props_)
+  {
+    oss << "    name: " << prop.first << ", "; 
+    oss << "type: " << std::get<0>(prop.second) << ", ";
+    oss << "arr: " << std::get<1>(prop.second) << '\n';
+  }
+  return oss;
+}
+
+} // namespace ply
---
+++ lib/data/ply_loader.h
@@ -0,0 +1,183 @@
+// *************************************************************
+// File:    ply_loader.h
+// Descr:   ply (ascii) format loader
+// Author:  Novoselov Anton @ 2018
+// URL:     https://github.com/ans-hub/ply_loader
+// *************************************************************
+
+#ifndef GC_PLY_LOADER_H
+#define GC_PLY_LOADER_H
+
+#include <map>
+#include <tuple>
+#include <vector>
+#include <iostream>
+#include <sstream>
+
+namespace ply {
+
+  // General aliases
+
+  using std::string;
+  using uint = std::size_t;
+
+//************************************************************************
+// Helper class represents header of ply file
+//************************************************************************
+
+  // Header aliases 
+
+  using SingleTuple = std::tuple<std::string, uint, uint>;
+  using SingleMap   = std::map<std::string, SingleTuple>;
+  using ListTuple   = std::tuple<std::string, uint>;
+  using ListMap     = std::map<std::string, ListTuple>;
+
+  // Header struct (storeds info about elements of ply file)
+
+  struct Header
+  {
+    int       pos_;
+    int       count_;
+    SingleMap single_props_;
+    ListMap   list_props_;
+
+  }; // struct Header
+
+//************************************************************************
+// Main class Loader for parsing ply files
+//************************************************************************
+
+  // Loader aliases
+
+  using Vector1s  = std::vector<std::string>;
+  using Vector1i  = std::vector<int>;
+  using Vector2c  = std::vector<std::vector<char>>;
+  using Vector1d  = std::vector<double>;
+  using Vector2d  = std::vector<Vector1d>;
+  using Vector3d  = std::vector<Vector2d>;
+  using Vector4d  = std::vector<Vector3d>;
+  using cVector3d = const Vector3d;
+  using cVector4d = const Vector4d;
+  using MapV      = std::map<std::string, Vector1s>;
+  using MapSI     = std::map<std::string, int>;
+  using MHeader   = std::map<std::string, Header>;
+  using cMHeader  = const std::map<std::string, Header>;
+
+  // Loader struct (parse info from stream)
+
+  struct Loader
+  {
+    bool        Load(std::istream&);  // add arg for name or some sort if id??
+    void        LoadHeader(std::istream&);
+    cMHeader&   GetHeader() const { return head_; }
+    Vector2d    GetLine(const std::string&, const Vector1s&);
+    Vector2d    GetList(const std::string&, const std::string&);
+    cVector4d&  GetRawData() const { return data_; }
+    cVector3d&  GetRawData(const std::string&);
+
+  private:
+    void LoadData(std::istream&);
+    void ClearData();
+
+    Vector2c  props_;
+    Vector1i  sizes_;
+    MHeader   head_;
+    Vector4d  data_;
+    MapSI     order_;
+    MapV      filter_;
+
+  }; // struct ply::Loader
+
+//************************************************************************
+// Exception helpers struct
+//************************************************************************
+
+struct Except : std::runtime_error
+{
+  Except(std::string&& msg) : std::runtime_error(msg.c_str()) { }
+  Except(const char* msg) : std::runtime_error(msg) { }
+
+}; // struct ply::Except
+
+//************************************************************************
+// Helper functions
+//************************************************************************
+
+namespace helpers {
+
+  void      RewindStream(std::istream&);
+  void      ThrowLoadDataError(int, int);
+
+  template<class T> // map
+  bool IsKeyAbsentInMap(const std::string& str, const T& map)
+  {
+    if (map.find(str) == map.end())
+      return true;
+    else
+      return false;
+  }
+
+  std::ostream& operator<<(std::ostream& , const Header&);
+  std::ostream& operator<<(std::ostream& , const MHeader&);
+
+} // namespace helpers
+
+} // namespace ply
+
+#endif  // GC_PLY_LOADER_H
+
+// Explanation:
+
+// #1: ply-file contains of "header_section" and "data_section". All data
+// described in header described in format like: element type, number of items
+// in this element and its properties (may be single or lists)
+
+// I.e., more imortant fields of header section:
+//   element vertex 26
+//   property float x
+//   property float y
+//   element face 12
+//   property list uchar uint ind
+//   property uint non
+//   property list uchar uint oth
+
+// Element "vertex" contains 26 items and has two single properties
+// Element "face" contains 12 items and has 2 list properies and 1 single
+
+// In data section first goes elements described first. I.e., one of the
+// element "vertex" (i.e. line 8) would looks like this:
+//  2.52 1.24
+// This means, that property x = 2.52, property y = 1.24
+
+// And one of the "face" (i.e. line 2) element would looks like this:
+//  3 9 8 7 100 4 2 1 1 1
+// This means, that current item of element "face" has following properties:
+// - list property "ind" contains of 3 elements (9,8,7)
+// - single property "non" == 100
+// - list property "oth" contains of 4 elements (2,1,1,1)
+
+// #2: terms inside Loader class:
+//  elem - element such as "vertex", "face", "edge", another
+//  item - item of current element (declared in header, i.e. "vertex 20")
+//  prop - properties of item (may be single or list properties)
+//  val  - concrete value of propertie
+
+// #3: how example described above would looks in Loader terms:
+//  data_[0][8][0]  ==  std::vector(size == 2) contains {2.52, 1.24}
+//  data_[1][2][0]  ==  std::vector(size == 3) contains {9,8,7}
+//  data_[1][2][1]  ==  std::vector(size == 1) contains {100}
+//  data_[1][2][0]  ==  std::vector(size == 4) contains {2,1,1,1}
+
+// #4: all data is stored in double float for simplicify. End user may
+// casts doubles to types described in current ply file.
+
+// #5: data member explanation
+//  data_  - stores loaded data in format data_[elem][item][prop][val]
+//  props_ - contains sequence of properties type (do define when we should
+//           push new propertie vector if we have list propery)
+//  sizes_ - contains count of elements 
+
+// #6: although construction of 4d vector is looks creepy, but all memory
+// allocations made 
+
+// # Todo:   l.SetFilter("vertex", {"x", "y", "z"});
